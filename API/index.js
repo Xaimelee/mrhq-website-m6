b78a26a19ce1d72c8adde889a0d195f98ad8bfb6
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2022-06-02 16:43:05 +1200

    API index.js and dog_food.js

--- API/index.js
+++ API/index.js
@@ -1,1 +1,17 @@
 console.log("Test")
+
+const mongoose = require('mongoose')
+const DogFood = require('./models/dog_food.js')
+
+const dbURI = "mongodb://admin:admin@mongo:27017/products?authSource=admin";
+mongoose.connect(dbURI, { useNewURLParser: true, useUnifiedTopology: true })
+    .then((result) => console.log("Connected"))
+    .catch((err) => console.log(err))
+
+DogFood.find()
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
