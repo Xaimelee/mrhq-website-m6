b60bd4b4d2e7bce55c7d5020fef88e2f1d45a26c
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2022-06-02 23:05:16 +1200

    .env added. API index.js added and working. React GetCollection added and working.

--- API/index.js
+++ API/index.js
@@ -1,17 +1,33 @@
-console.log("Test")
-
+const express = require("express")
 const mongoose = require('mongoose')
 const DogFood = require('./models/dog_food.js')
 
-const dbURI = "mongodb://admin:admin@mongo:27017/products?authSource=admin";
-mongoose.connect(dbURI, { useNewURLParser: true, useUnifiedTopology: true })
+const server = express()
+const port = process.env.PORT || 8999
+const url = "mongodb://admin:admin@mongo:27017/products?authSource=admin"
+
+server.use(express.json())
+
+mongoose.connect(url, { useNewURLParser: true, useUnifiedTopology: true })
     .then((result) => console.log("Connected"))
     .catch((err) => console.log(err))
 
-DogFood.find()
-    .then((result) => {
-        console.log(result)
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+server.listen(port, () => {
+    console.log("Listening: Port", + port)
+})
+
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+server.get("/getDogFood", (req, res) => {
+    DogFood.find()
+        .then((result) => {
+            res.send(result)
+            return result
+        })
+        .catch((err) => {
+            return err
+        })
+})
--- React/src/index.js
+++ React/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import GetCollection from './GetCollection';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <GetCollection collection = "getDogFood"/>
   </React.StrictMode>
 );
 
