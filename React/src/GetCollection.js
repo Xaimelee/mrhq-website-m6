7816bd2fddaedf0430bfeb90855f74bc7ad591ee
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2022-06-03 00:13:48 +1200

    Cleared React template.

--- React/src/GetCollection.js
+++
@@ -1,9 +0,0 @@
-function GetCollection({collection}) {
-    let url = 'http://127.0.0.1:9999/' + collection
-
-    fetch(url)
-        .then(res => res.json())
-        .then(data => console.log(data))
-}
-
-export default GetCollection
