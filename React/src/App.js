992cbb1aa4789d370065f2b8af842f7435f37f63
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2022-06-06 21:32:43 +1200

    DisplayCollection commented out of App.

--- React/src/App.js
+++ React/src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
 import React from 'react';
-import DisplayCollection from './Frontend/DisplayCollection';
+// import DisplayCollection from './Frontend/DisplayCollection';
+// <DisplayCollection url = "getDogFood"/>
 
 function App() {
   return (
     <div className="App">
-      <DisplayCollection url = "getDogFood"/>
+      
     </div>
   );
 }
