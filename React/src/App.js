31dcce682cdefb35de64cbe8ea9f0d822202316d
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2022-06-09 23:55:57 +1200

    Product page route. SearchButton component added.

--- React/src/App.js
+++ React/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './Pages/Layout';
 import Homepage from './Pages/Homepage';
+import Products from './Pages/Products';
 // import DisplayCollection from './Frontend/DisplayCollection';
 // <DisplayCollection url = "getDogFood"/>
 
@@ -12,6 +13,9 @@ function App() {
       <Routes>
         <Route path = "/" element = {<Layout />}>
           <Route index element = {<Homepage />} />
+        </Route>
+        <Route path = "/Products" element = {<Layout />}>
+          <Route index element = {<Products />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ React/src/Pages/Products.jsx
@@ -0,0 +1,7 @@
+function Products() {
+    return (
+        <div>Hello this is ze productos</div>
+    )
+}
+
+export default Products
